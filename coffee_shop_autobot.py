aefe042b105369368ff470e46350cd6db0b74b49
HTP28 <phat.ho1692@gmail.com>
2023-11-25 10:15:00 +0700

    Update to check the evil name

--- coffee_shop_autobot.py
+++ coffee_shop_autobot.py
@@ -9,13 +9,16 @@ print("Hello, welcome to " + COFFEE_SHOP_NAME +" Coffee!!!\n")
 # ask for the customer name
 name = input("What's your name?\n")
 
-if name == "Ben":
+CONSIDER_EVIL_NAME = ["Ben", "Patricia", "Loki"]
+
+if name in CONSIDER_EVIL_NAME:
     evil_status = input("Are you evil?\n")
-    if evil_status == "Yes":
-        print("\nYou're not welcome here Evil Ben!! Get out!!\n")
+    good_deeds = int(input("How many good deeds have you done today?\n"))
+    if evil_status == "Yes" and good_deeds < 4:
+        print("\nYou're not welcome here Evil " + name + "!! Get out!!\n")
         sys.exit(0)
     else:
-        print("Oh, so you're on of those good Bens, Come on in!!")
+        print("\nOh, so you're on of those good " + name + ", Come on in!!\n")
 else:
     print("\nHello " + name + ", thank you so much for coming in today.\n")
 
@@ -29,20 +32,26 @@ while True:
     order = input("\nWhat would you like?\n")
 
     if order in MENU:
-        if order == "Black Coffee":
+        if order == MENU[0]:
             PRICE = 3
-        elif order == "Espresso":
+        elif order == MENU[1]:
             PRICE = 8
-        elif order == "Latte":
-            PRICE = 9
-        elif order == "Cappuccino":
+        elif order == MENU[2]:
+            add_cream = input("\nDo you want to add cream?\n")
+            if add_cream == "Yes":
+                PRICE = 11
+            else:
+                PRICE = 9
+        elif order == MENU[3]:
             PRICE = 12
-        elif order == "Frappuccino":
+        elif order == MENU[4]:
             PRICE = 13
 
         break
-    else:
-        print("\nSorry, we don't sell that. Please choose from the menu.")
+
+    print("\nSorry, we don't sell that. Please choose from the menu.")
+    print("Here is the menu again")
+    print(MENU)
 
 # ask for the quantity the customer wants to order
 quantity = input("\nHow many coffees would you like?\n")
